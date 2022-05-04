111194779b5010462a31ac944e441d2ed921c650
Sara <saragigam97@gmail.com>
2022-05-04 20:46:45 +0200

    commit

---
+++ Main.py
@@ -0,0 +1,34 @@
+from Nau import Nau
+
+def menu():
+    print("w- Moure dreta")
+    print("a- Moure equerre")
+    print("d- Moure amunt")
+    print("s- Moure moure avall")    
+    print("0- Sortir")
+
+def main():
+
+    menu()
+    
+    nau = Nau(0,0)
+
+    sortir=False
+    while not sortir:
+        op = input('Entra una opció')
+        if op=='d':
+            nau.moure_dreta()
+        elif op=='a':
+            print('moure a l''esquerre')
+        elif op=='w':
+            print('moure a amunt')
+        elif op=='s':
+            print('moure a avall')
+        elif op=='0':
+            sortir=True
+            print("Has sortit de l'avió")
+        
+        nau.mostrar_nau()
+        
+if __name__ == "__main__":
+    main()
---
+++ Nau.py
@@ -0,0 +1,22 @@
+class Nau:
+
+    def __init__(self,x,y):        
+        self.x=x
+        self.y=y
+        self.incr=1
+    
+    def moure_dreta(self):
+        pass
+        
+    def moure_esquerre(self):
+        pass
+
+    def moure_amunt(self):
+        pass
+        
+    def moure_avall(self):
+        pass    
+        
+    def mostrar_nau(self):
+        pass
+
